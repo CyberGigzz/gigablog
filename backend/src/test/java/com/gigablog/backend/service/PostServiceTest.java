73e81566f8ddf3d6dd75f960d4451ed225255e06
Giga Katamadze <gigakatamadze0@gmail.com>
2025-03-17 16:44:59 +0400

    added some unit tests

--- backend/src/test/java/com/gigablog/backend/service/PostServiceTest.java
+++ backend/src/test/java/com/gigablog/backend/service/PostServiceTest.java
@@ -1,6 +1,7 @@
 package com.gigablog.backend.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -54,5 +55,7 @@ public class PostServiceTest {
         assertEquals(content, createdPost.getContent());
         verify(postRepository, times(1)).save(any(Post.class));
     }
+
+    
 
 }
