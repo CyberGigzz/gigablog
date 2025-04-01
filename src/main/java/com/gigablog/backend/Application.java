dbe77c0d11f1e8ecb4543d70d374762188c059cf
Giga Katamadze <gigakatamadze0@gmail.com>
2025-04-01 17:07:31 +0400

    Testing CI/CD

--- src/main/java/com/gigablog/backend/Application.java
+++ src/main/java/com/gigablog/backend/Application.java
@@ -3,18 +3,18 @@ package com.gigablog.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		// try {
-        //     Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        //     dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-        // } catch (Exception e) {
-        //     System.out.println("No .env file found or error loading it. Relying on system environment variables.");
-        // }
+		try {
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+            dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        } catch (Exception e) {
+            System.out.println("No .env file found or error loading it. Relying on system environment variables.");
+        }
 		SpringApplication.run(Application.class, args);
 	}
 	
