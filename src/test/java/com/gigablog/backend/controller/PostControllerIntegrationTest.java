b163b9a89b47c76741b86fe133453dd4f1eb567a
Giga Katamadze <gigakatamadze0@gmail.com>
2025-04-01 18:33:49 +0400

    Testing CI/CD

--- src/test/java/com/gigablog/backend/controller/PostControllerIntegrationTest.java
+++ src/test/java/com/gigablog/backend/controller/PostControllerIntegrationTest.java
@@ -34,47 +34,47 @@ public class PostControllerIntegrationTest {
                 .andExpect(jsonPath("$[0].createdAt").exists());
     }
 
-    // @Test
-    // void shouldGetPostById() throws Exception {
-    //     mockMvc.perform(get("/api/v1/posts/20")
-    //             .contentType(MediaType.APPLICATION_JSON))
-    //             .andExpect(status().isOk())
-    //             .andExpect(jsonPath("$.title").exists())
-    //             .andExpect(jsonPath("$.content").exists())
-    //             .andExpect(jsonPath("$.createdAt").exists());
-    // }
+    @Test
+    void shouldGetPostById() throws Exception {
+        mockMvc.perform(get("/api/v1/posts/20")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").exists())
+                .andExpect(jsonPath("$.content").exists())
+                .andExpect(jsonPath("$.createdAt").exists());
+    }
 
-    // @Test
-    // void shouldCreatePost() throws Exception {
-    //     String postJson = "{\"title\": \"Test Post Title\", \"content\": \"Test Post Content\"}";
+    @Test
+    void shouldCreatePost() throws Exception {
+        String postJson = "{\"title\": \"Test Post Title\", \"content\": \"Test Post Content\"}";
 
-    //     mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/posts")
-    //             .contentType(MediaType.APPLICATION_JSON)
-    //             .content(postJson))
-    //             .andExpect(status().isCreated()) // Or status().isOk() depending on your controller
-    //             .andExpect(jsonPath("$.title").value("Test Post Title"))
-    //             .andExpect(jsonPath("$.content").value("Test Post Content"))
-    //             .andExpect(jsonPath("$.createdAt").exists());
-    // }
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(postJson))
+                .andExpect(status().isCreated()) // Or status().isOk() depending on your controller
+                .andExpect(jsonPath("$.title").value("Test Post Title"))
+                .andExpect(jsonPath("$.content").value("Test Post Content"))
+                .andExpect(jsonPath("$.createdAt").exists());
+    }
 
-    // @Test
-    // void shouldUpdatePost() throws Exception {
-    //     String postJson = "{\"title\": \"Test Post Title Updated\", \"content\": \"Test Post Content Updated\"}";
+    @Test
+    void shouldUpdatePost() throws Exception {
+        String postJson = "{\"title\": \"Test Post Title Updated\", \"content\": \"Test Post Content Updated\"}";
 
-    //     mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/posts/20")
-    //             .contentType(MediaType.APPLICATION_JSON)
-    //             .content(postJson))
-    //             .andExpect(status().isOk())
-    //             .andExpect(jsonPath("$.title").value("Test Post Title Updated"))
-    //             .andExpect(jsonPath("$.content").value("Test Post Content Updated"))
-    //             .andExpect(jsonPath("$.updatedAt").exists());
-    // }
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/posts/20")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(postJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Test Post Title Updated"))
+                .andExpect(jsonPath("$.content").value("Test Post Content Updated"))
+                .andExpect(jsonPath("$.updatedAt").exists());
+    }
 
-    // @Test
-    // void shouldDeletePost() throws Exception {
-    //     mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/posts/20")
-    //             .contentType(MediaType.APPLICATION_JSON))
-    //             .andExpect(status().isNoContent());
-    // }
+    @Test
+    void shouldDeletePost() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/posts/20")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
 
 }
